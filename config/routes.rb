192565697b3041fca49859badef391eb1ebc5d4a
abdelp <juniorperezpy@gmail.com>
2020-11-14 15:34:37 -0300

    Add Accounts scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  resources :accounts
   resources :homes
   devise_for :users, controllers: {
     sessions: 'users/sessions'
+  }
+
+  devise_for :admins, controllers: {
+    sessions: 'amins/sessions'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#index'
